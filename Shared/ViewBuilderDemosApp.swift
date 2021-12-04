4d8069e2a9df1e80328e1c74d974b97ebd1f2ee1
Rudi Farkas <rudi.farkas@gmail.com>
2021-12-04 14:34:28 +0100

    Initial Commit

---
+++ Shared/ViewBuilderDemosApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewBuilderDemosApp.swift
+//  Shared
+//
+//  Created by Rudolf Farkas on 04.12.21.
+//
+
+import SwiftUI
+
+@main
+struct ViewBuilderDemosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
